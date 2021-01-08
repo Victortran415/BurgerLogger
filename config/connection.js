53a4bf36793eb612215641ab6f34bd1f5775bf81
Victor Tran <victortran415@gmail.com>
2021-01-07 16:17:32 -0800

    adding mysql.createConnection information

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,20 @@
 const mysql = require("mysql");
+const { connect } = require("../controllers/burgers_controller");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "burgerDB",
+});
 
-
+connection.connect((err) => {
+    if (err){
+        console.error(`Error Connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`Connected as ID ${connection.threadId}`)
+})
 
 module.exports = connection;
