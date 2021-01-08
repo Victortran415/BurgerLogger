e1dfafa08214c3f3fd0cde14d51288cfe47c139e
Victor Tran <victortran415@gmail.com>
2021-01-08 12:48:42 -0800

    added code to model

--- model/burger.js
+++ model/burger.js
@@ -1,11 +1,16 @@
+const { insertOne } = require("../config/orm.js");
 const orm = require("../config/orm.js");
 
 const burgerJS = {
     selectAll(cb) {
         orm.selectAll("burgers", (res) => cb(res))
+    },
+    insertOne(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, (res) => cb(res));
+    },
+    updateOne(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, (res) => cb(res));
     }
-
-    
 }
 
 module.exports = burgerJS;
