38adc3a2663006ef0ffd6a94ff36cf66c0dca1d7
Victor Tran <victortran415@gmail.com>
2021-01-14 17:26:50 -0800

    trying to update connection with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,14 @@ const mysql = require("mysql");
 
 
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "Password",
-        database: "burgerDB",
-    });
-}
+
+const connection = mysql.createConnection({
+    host: "lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "c6txk884y2kazpg3",
+    password: "jtdhgfb9aal1pizs",
+    database: "lpv5pcjpbqj92iyp",
+});
 
 connection.connect((err) => {
     if (err){
